2cce98e0b4ec84876b2e699edb69f45ffb2ad431
Aryan S Shandilya <aryanacc28@gmail.com>
2025-12-05 14:37:42 +0530

    second commit

--- brain.js
+++ brain.js
@@ -37,10 +37,10 @@ const start = async () => {
     await mongoose.connect(process.env.MONGO_URI);
   console.log("CONNECTED TO DB");
   
-  const port = 4000;
+  const port = process.env.PORT || 4000;;
 
   app.listen(port, () => {
-    console.log("Server running at http://localhost:4000");
+    console.log(`Server running at http://localhost:${port}`);
   });
   }
   catch(err){
